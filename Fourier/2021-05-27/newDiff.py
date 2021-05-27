9634c00f70f62cd898987148ce58a5194e0571ab
kakeru-516 <ue.0516@gmail.com>
2021-05-27 20:29:21 +0900

    Fix diff.py

---
+++ Fourier/2021-05-27/newDiff.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+
+N = 128 # サンプル数
+
+img = cv2.imread('../img/0.png', cv2.IMREAD_COLOR)
+# print('y(縦) = ' + str(img.shape[0]))
+# print('x(横) = ' + str(img.shape[1]))
+# print('(チャネル数) = ' + str(img.shape[2]))
+
+height = img.shape[0]
+width = img.shape[1]
+frame = 0
+
+img = np.zeros((N, height, width), dtype=np.uint8)
+
+# 画像の読み込み
+for i in range(0, 128):
+    img[frame] = cv2.imread('../img/' + str(i) + '.png', cv2.IMREAD_GRAYSCALE)
+    frame += 1
+
+
+mask = np.zeros((height, width), dtype='uint64')
+
+for i in range(1, N):
+    diff = cv2.absdiff(img[i - 1], img[i]) * 0.1
+    diff = diff.astype(np.uint64)
+    mask = mask + diff
+
+mask = mask.astype(np.uint8)
+threshold = int(mask.max() * 0.2)
+ret, mask = cv2.threshold(mask, threshold, 255, cv2.THRESH_BINARY)
+
+mask = cv2.medianBlur(mask, ksize=13)
+cv2.imwrite('diff-0-128.png', mask)
